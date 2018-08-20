12e8f436adbf8050efd83530d2570362fd50938e
fermadeiral <fer.madeiral@gmail.com>
2018-08-20 04:09:23 +0200

    Bug commit from aicis/fresco This commit is based on the source code from the following commit: http://github.com/aicis/fresco/commit/1beb18a142d7a83dac60079d4c47b213d13b4303 The mentioned commit triggered the following Travis build: http://travis-ci.org/aicis/fresco/builds/329090508.

---
+++ tools/fixedpointarithmetic/src/test/java/dk/alexandra/fresco/fixedpoint/basic/TestDummyArithmeticProtocolSuite.java
@@ -0,0 +1,91 @@
+package dk.alexandra.fresco.fixedpoint.basic;
+
+import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
+import dk.alexandra.fresco.suite.dummy.arithmetic.AbstractDummyArithmeticTest;
+import org.junit.Test;
+
+
+public class TestDummyArithmeticProtocolSuite extends AbstractDummyArithmeticTest {
+
+  @Test
+  public void test_Input_Sequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestInput<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+
+  @Test
+  public void test_Known() throws Exception {
+    runTest(new BasicArithmeticTests.TestKnown<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+  @Test
+  public void test_AddKnown() throws Exception {
+    runTest(new BasicArithmeticTests.TestAddKnown<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+  @Test
+  public void test_AddSecret() throws Exception {
+    runTest(new BasicArithmeticTests.TestAddSecret<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+  @Test
+  public void test_SubtractSecret() throws Exception {
+    runTest(new BasicArithmeticTests.TestSubtractSecret<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+  @Test
+  public void test_SubKnown() throws Exception {
+    runTest(new BasicArithmeticTests.TestSubKnown<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+  @Test
+  public void test_SubKnown2() throws Exception {
+    runTest(new BasicArithmeticTests.TestSubKnown2<>(), EvaluationStrategy.SEQUENTIAL,
+        2);
+  }
+  
+/*
+  @Test
+  public void test_OutputToTarget_Sequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestOutputToSingleParty<>(), EvaluationStrategy.SEQUENTIAL,
+        2, defaultMod, true);
+    assertThat(performanceLoggers.get(1).getLoggedValues()
+        .get(NetworkLoggingDecorator.NETWORK_TOTAL_BYTES), is((long)0));
+  }
+
+  @Test
+  public void test_AddPublicValue_Sequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestAddPublicValue<>(), EvaluationStrategy.SEQUENTIAL,
+        1);
+  }
+
+  @Test
+  public void test_KnownSInt_Sequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestKnownSInt<>(), EvaluationStrategy.SEQUENTIAL,
+        1);
+  }
+
+  @Test
+  public void test_MultAndAdd_Sequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestSimpleMultAndAdd<>(), EvaluationStrategy.SEQUENTIAL,
+        1);
+  }
+
+  @Test
+  public void testSumAndOutputSequential() throws Exception {
+    runTest(new BasicArithmeticTests.TestSumAndMult<>(), EvaluationStrategy.SEQUENTIAL,
+        1);
+  }
+
+  @Test
+  public void testSumAndProduct() throws Exception {
+    runTest(new TestSumAndProduct<>(), EvaluationStrategy.SEQUENTIAL,
+        1);
+  }
+*/
+}
