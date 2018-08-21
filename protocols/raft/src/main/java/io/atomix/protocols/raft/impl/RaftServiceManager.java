fe5c0d95fa50742aaca79a1db3aaa672e1e22f57
fermadeiral <fer.madeiral@gmail.com>
2018-08-21 17:53:31 +0200

    Human patch from atomix/atomix This commit is based on the source code from the following commit: http://github.com/atomix/atomix/commit/2f30188738f51eba512c20f9e1f9b96ec3d3ca66 The mentioned commit triggered the following Travis build: http://travis-ci.org/atomix/atomix/builds/332448781.

--- protocols/raft/src/main/java/io/atomix/protocols/raft/impl/RaftServiceManager.java
+++ protocols/raft/src/main/java/io/atomix/protocols/raft/impl/RaftServiceManager.java
@@ -206,8 +206,12 @@ public class RaftServiceManager implements AutoCloseable {
     // If snapshots exist for the prior index, iterate through snapshots and populate services/sessions.
     if (snapshots != null) {
       for (Snapshot snapshot : snapshots) {
+        DefaultServiceContext service = null;
         try (SnapshotReader reader = snapshot.openReader()) {
-          restoreService(reader);
+          service = restoreService(reader);
+        }
+        if (service != null) {
+          service.installSnapshot(index);
         }
       }
     }
@@ -218,7 +222,7 @@ public class RaftServiceManager implements AutoCloseable {
    *
    * @param reader the snapshot reader
    */
-  private void restoreService(SnapshotReader reader) {
+  private DefaultServiceContext restoreService(SnapshotReader reader) {
     ServiceId serviceId = ServiceId.from(reader.readLong());
     ServiceType serviceType = ServiceType.from(reader.readString());
     String serviceName = reader.readString();
@@ -227,10 +231,11 @@ public class RaftServiceManager implements AutoCloseable {
     logger.debug("Restoring service {} {}", serviceId, serviceName);
     DefaultServiceContext service = initializeService(serviceId, serviceType, serviceName);
     if (service == null) {
-      return;
+      return null;
     }
 
     restoreSessions(reader, service);
+    return service;
   }
 
   /**
@@ -509,8 +514,10 @@ public class RaftServiceManager implements AutoCloseable {
     // If the session is null, return an UnknownSessionException. Commands applied to the state machine must
     // have a session. We ensure that session register/unregister entries are not compacted from the log
     // until all associated commands have been cleaned.
+    // Note that it's possible for a session to be unknown if a later snapshot has been taken, so we don't want
+    // to log warnings here.
     if (session == null) {
-      logger.warn("Unknown session: " + entry.entry().session());
+      logger.debug("Unknown session: " + entry.entry().session());
       return Futures.exceptionalFuture(new RaftException.UnknownSession("unknown session: " + entry.entry().session()));
     }
 
--- protocols/raft/src/main/java/io/atomix/protocols/raft/proxy/impl/RaftProxyInvoker.java
+++ protocols/raft/src/main/java/io/atomix/protocols/raft/proxy/impl/RaftProxyInvoker.java
@@ -17,7 +17,6 @@ package io.atomix.protocols.raft.proxy.impl;
 
 import io.atomix.protocols.raft.RaftError;
 import io.atomix.protocols.raft.RaftException;
-import io.atomix.protocols.raft.RaftException.ProtocolException;
 import io.atomix.protocols.raft.operation.RaftOperation;
 import io.atomix.protocols.raft.protocol.CommandRequest;
 import io.atomix.protocols.raft.protocol.CommandResponse;
@@ -50,8 +49,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 final class RaftProxyInvoker {
   private static final int[] FIBONACCI = new int[]{1, 1, 2, 3, 5};
   private static final Predicate<Throwable> EXCEPTION_PREDICATE = e ->
-      e instanceof ProtocolException
-          || e instanceof ConnectException
+      e instanceof ConnectException
           || e instanceof TimeoutException
           || e instanceof ClosedChannelException;
   private static final Predicate<Throwable> CLOSED_PREDICATE = e ->
@@ -194,6 +192,17 @@ final class RaftProxyInvoker {
   }
 
   /**
+   * Resubmits pending commands.
+   */
+  public void reset() {
+    context.execute(() -> {
+      for (OperationAttempt attempt : attempts.values()) {
+        attempt.retry();
+      }
+    });
+  }
+
+  /**
    * Closes the submitter.
    *
    * @return A completable future to be completed with a list of pending operations.
@@ -280,7 +289,10 @@ final class RaftProxyInvoker {
      * @param t The exception with which to fail the attempt.
      */
     public void fail(Throwable t) {
-      complete(t);
+      sequence(null, () -> {
+        state.setCommandResponse(request.sequenceNumber());
+        future.completeExceptionally(t);
+      });
 
       // If the session has been closed, update the client's state.
       if (CLOSED_PREDICATE.test(t)) {
@@ -343,8 +355,9 @@ final class RaftProxyInvoker {
         else if (response.error().type() == RaftError.Type.COMMAND_FAILURE) {
           resubmit(response.lastSequenceNumber(), this);
         }
-        // If an APPLICATION_ERROR occurred, complete the request exceptionally with the error message.
-        else if (response.error().type() == RaftError.Type.APPLICATION_ERROR) {
+        // If a PROTOCOL_ERROR or APPLICATION_ERROR occurred, complete the request exceptionally with the error message.
+        else if (response.error().type() == RaftError.Type.PROTOCOL_ERROR
+            || response.error().type() == RaftError.Type.APPLICATION_ERROR) {
           complete(response.error().createException());
         }
         // If the client is unknown by the cluster, close the session and complete the operation exceptionally.
