8bc159aeef7e05b4e0a386b8391d5adb5cb00ca3
fermadeiral <fer.madeiral@gmail.com>
2018-09-01 02:37:31 +0200

    Human patch from languagetool-org/languagetool This commit is based on the source code from the following commit: http://github.com/languagetool-org/languagetool/commit/2580e69f10098005ee366be04844f4dc11bab166 The mentioned commit triggered the following Travis build: http://travis-ci.org/languagetool-org/languagetool/builds/356974000.

--- languagetool-standalone/src/main/java/org/languagetool/gui/LanguageComboBoxModel.java
+++ languagetool-standalone/src/main/java/org/languagetool/gui/LanguageComboBoxModel.java
@@ -58,7 +58,7 @@ class LanguageComboBoxModel extends DefaultComboBoxModel<LanguageAdapter> {
       ArrayList<Language> ext = new ArrayList<>(external);
       Collections.sort(ext, comparator);
       for (Language l : ext) {
-    	model.addElement(new LanguageAdapter(l));
+    	 model.addElement(new LanguageAdapter(l));
       }
     }
     // the original list is unmodifiable
@@ -66,18 +66,6 @@ class LanguageComboBoxModel extends DefaultComboBoxModel<LanguageAdapter> {
     Collections.sort(internal, comparator);
     for (Language l : internal) {
       if (includeHidden || !l.isHiddenFromGui()) {
-    	  String removeOptionText=null;
-    	  String warningMsg=null;
-    	  ResourceBundle resourceBundleTools=JLanguageTool.getMessageBundle(l);
-    	  try{
-    		  removeOptionText=resourceBundleTools.getString("guiRemoveOptionText");
-    		  warningMsg=resourceBundleTools.getString("guiRemoveWarningMsg");
-    	  	  System.out.println(l+"\t\t\t"+ removeOptionText+"\t\t\t"+warningMsg);
-      }
-    	  catch(Exception ex)
-    	  {
-    		  System.out.println(ex);
-    	  }
     	  model.addElement(new LanguageAdapter(l));
       }
     }
