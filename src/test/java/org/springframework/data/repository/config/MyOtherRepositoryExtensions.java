bebd331143150f5e1a90080c2ce1ccbc5a27a03d
fermadeiral <fer.madeiral@gmail.com>
2018-10-10 06:59:51 +0200

    Changes in the tests This commit is based on the source code from the following commit: http://github.com/spring-projects/spring-data-commons/commit/edf2b62250e1de7e8e4484f4ec64768454a5542b The mentioned commit triggered the following Travis build: http://travis-ci.org/spring-projects/spring-data-commons/builds/206643702.

--- src/test/java/org/springframework/data/repository/config/MyOtherRepositoryExtensions.java
+++ src/test/java/org/springframework/data/repository/config/MyOtherRepositoryExtensions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,6 @@
  */
 package org.springframework.data.repository.config;
 
-import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.config.AnnotationRepositoryConfigurationSourceUnitTests.Person;
-
-interface MyOtherRepository extends Repository<Person, Long>, MyOtherRepositoryExtensions {
-
+public interface MyOtherRepositoryExtensions {
+	String getImplementationId();
 }
--- src/test/java/org/springframework/data/repository/config/excluded/MyOtherRepositoryImpl.java
+++ src/test/java/org/springframework/data/repository/config/excluded/MyOtherRepositoryImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.data.repository.config;
+package org.springframework.data.repository.config.excluded;
 
-import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.config.AnnotationRepositoryConfigurationSourceUnitTests.Person;
+import org.springframework.data.repository.config.MyOtherRepositoryExtensions;
 
-interface MyOtherRepository extends Repository<Person, Long>, MyOtherRepositoryExtensions {
+public class MyOtherRepositoryImpl implements MyOtherRepositoryExtensions {
 
+	@Override
+	public String getImplementationId() {
+		return getClass().getName();
+	}
 }
