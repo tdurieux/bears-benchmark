c98346c975f205f8f51e7a2a51deafb408452d62
fermadeiral <fer.madeiral@gmail.com>
2018-10-13 15:09:07 +0200

    Changes in the tests This commit is based on the source code from the following commit: http://github.com/mforoni/jcoder/commit/f78219d64c358266a3b3c34336e37053136ccd2c The mentioned commit triggered the following Travis build: http://travis-ci.org/mforoni/jcoder/builds/440995156.

--- jcoder/src/test/java/com/github/mforoni/jcoder/NamesTest.java
+++ jcoder/src/test/java/com/github/mforoni/jcoder/NamesTest.java
@@ -1,14 +1,12 @@
 package com.github.mforoni.jcoder;
 
-import com.github.mforoni.jcoder.Names;
 import junit.framework.TestCase;
 
 public class NamesTest extends TestCase {
-
-	public final void testOfField() {
-		assertEquals("test", Names.ofField(" Test"));
-		assertEquals("teST", Names.ofField(" TeST"));
-		assertEquals("tEST", Names.ofField(" TEST"));
-		assertEquals("veryLongTextToBeConverted", Names.ofField(" veRY Long texT_to Be ConVERTED"));
-	}
+  public final void testOfField() {
+    assertEquals("test", Names.ofField(" TeST"));
+    assertEquals("test", Names.ofField(" 1_TEST"));
+    assertEquals("veryLongTextToBeConverted", Names.ofField(" veRY Long texT_to Be ConVERTED"));
+    assertEquals("veryLongTextToBeConverted", Names.ofField(" veRY . Long texT_ to Be ConVERTED"));
+  }
 }
