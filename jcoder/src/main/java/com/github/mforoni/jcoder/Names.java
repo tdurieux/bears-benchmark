58b225ea9b16076c53ef9f11560f1062e770538d
fermadeiral <fer.madeiral@gmail.com>
2018-10-13 15:09:07 +0200

    Human patch from mforoni/jcoder This commit is based on the source code from the following commit: http://github.com/mforoni/jcoder/commit/f78219d64c358266a3b3c34336e37053136ccd2c The mentioned commit triggered the following Travis build: http://travis-ci.org/mforoni/jcoder/builds/440995156.

--- jcoder/src/main/java/com/github/mforoni/jcoder/Names.java
+++ jcoder/src/main/java/com/github/mforoni/jcoder/Names.java
@@ -12,45 +12,61 @@ import com.google.common.base.Strings;
  * 
  * @author Foroni
  */
+@Beta
 public final class Names {
+  private Names() {
+    throw new AssertionError();
+  }
+
   /**
    * Returns the conventional field name from the given string i.e. the lower camel case version. If
    * the input contains spaces the string is trimmed and all consecutive spaces are replaced with
-   * just one space. Then this normalized string is converted to lower case according to the
-   * following:
-   * <ul>
-   * <li>if the normalized string is alphabetic and all in upper case returns the string in lower
-   * case</li>
-   * <li>if the normalized string is alphabetic and the first character is in upper case return the
-   * string decapitalized</li>
-   * <li>if the normalized string contains {@code ' '} or {@code '_'} then conversion to lower camel
-   * case is performed according to converted to
-   * {@link JStrings#lowerCamelCase(String, char, char...)}</li>
-   * <li>if no previous conditions are satisfied throws an {@code IllegalArgumentException}.</li>
-   * </ul>
+   * just one space. Then this normalized string is converted to lower camel case considering the
+   * characters _ . - and space as word separators.
    * 
    * @param s the string to convert in a field name
    * @return the conventional field name from the given string.
-   * @throws IllegalArgumentException if the given string is null or empty
+   * @throws IllegalArgumentException if the given string is null or empty or does not contains at
+   *         least one alphabetic character
    */
-  @Beta
   @Nonnull
   public static String ofField(@Nonnull final String s) throws IllegalArgumentException {
     if (Strings.isNullOrEmpty(s)) {
       throw new IllegalArgumentException("The specified string must be not null and not empty");
     }
+    final int indexOfFirstLetter = indexOfFirstLetter(s);
+    if (indexOfFirstLetter == -1) {
+      throw new IllegalArgumentException(
+          "The specified string must contains at least one alphabetic character");
+    }
+    // removing non alphabetic initial characters
+    final String ss = s.substring(indexOfFirstLetter);
     final String normalized =
-        JStrings.contains(s, ' ') ? JStrings.removeConsecutiveSpaces(s.trim()) : s;
+        JStrings.contains(ss, ' ') ? JStrings.removeConsecutiveSpaces(ss.trim()) : ss;
     final String[] split = JStrings.split(normalized, ' ', '_', '.', '-');
     final List<String> parts = new ArrayList<>(split.length);
-    final String start =
-        split.length > 1 ? split[0].toLowerCase() : JStrings.decapitalize(split[0]);
-    // FIXME if start.chartAt(0) is not letter
-    parts.add(start);
+    parts.add(split[0].toLowerCase());
     for (int i = 1; i < split.length; i++) {
-      final String word = split[i];
-      parts.add(JStrings.capitalize(word.toLowerCase()));
+      if (split[i].length() > 0) {
+        parts.add(JStrings.capitalize(split[i].toLowerCase()));
+      }
     }
     return JStrings.concat(parts);
+  }
+
+  private static int indexOfFirstLetter(@Nonnull final String s) {
+    for (int i = 0; i < s.length(); i++) {
+      final char c = s.charAt(i);
+      if (Character.isAlphabetic(c)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  @Nonnull
+  public static String ofClass(@Nonnull final String s) {
+    final String ofField = Names.ofField(s);
+    return JStrings.capitalize(ofField.toLowerCase());
   }
 }
